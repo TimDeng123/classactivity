f170aedc379f9c8dfd4dbc21e28962692348e44c
Tim Deng <qq2574423608@gmail.com>
2022-11-24 16:53:25 +1100

    Firbase config

---
+++ src/config/FirebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA4pLMwdbSktrm3UpYxzScVr_Ogmf3xkU8",
+    authDomain: "bookreview-4eab2.firebaseapp.com",
+    projectId: "bookreview-4eab2",
+    storageBucket: "bookreview-4eab2.appspot.com",
+    messagingSenderId: "1053321229157",
+    appId: "1:1053321229157:web:9e6a81f851c031b354678c"
+  };
+  
